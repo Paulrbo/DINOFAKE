b056d2dededde2e8616428af939625ba4e0cc403
Paul Rbo <herbeaupaul@gmail.com>
2020-11-20 16:00:37 +0100

    camarche (le menu)

--- script.js
+++ script.js
@@ -1,55 +1,4 @@
-$(function () {
-    /*$('.scroll-down').click(function() {
-        $('html, body').animate({ scrollTop: $('section.ok').offset().top }, 'slow');
-        return false;
-    });*/
-    $("#home_to_chrono").click(function() { // quand l'utilisateur clique dans la case
-
-        valeur = location.hash.substr(1);
-        console.log(valeur);
-        if (valeur == "chrono_section") {
-            $("#chrono_section").show("fast");
-            $("#home_to_chrono").hide("fast");
-        }
-    });
-    $("#chrono_to_stakes").click(function() { // quand l'utilisateur clique dans la case
-
-         valeur = location.hash.substr(1);
-        console.log(valeur);
-        if (valeur == "stakes_section") {
-            $("#stakes_section").show("fast");
-            $("#chrono_to_stakes").hide("fast");
-        }
-    });
-    $("#stakes_to_answer").click(function() { // quand l'utilisateur clique dans la case
-
-         valeur = location.hash.substr(1);
-        console.log(valeur);
-        if (valeur == "answer_section") {
-            $("#answer_section").show("fast");
-            $("#stakes_to_answer").hide("fast");
-        }
-    });
-    $("#answer_to_map").click(function() { // quand l'utilisateur clique dans la case
-
-        valeur = location.hash.substr(1);
-        console.log(valeur);
-        if (valeur == "map_section") {
-            $("#map_section").show("fast");
-            $("#answer_to_map").hide("fast");
-        }
-    });
-    /*  $("#map_to_home").click(function() { // quand l'utilisateur clique dans la case
-          valeur = this.href;
-          valeur = valeur.split("index.html");
-          console.log(valeur);
-          if (valeur[1] == "#home_section") {
-              $("#map_section").hide("fast");
-              $("#answer_section").hide("fast");
-              $("#stakes_section").hide("fast");
-              $("#chrono_section").hide("fast");
-          }
-      });*/
+$(function() {
     $("#home_to_chrono").hover(function() {
         $("#defilement").show();
         $("#defilement").fadeOut(2000);
@@ -60,3 +9,28 @@ $(function () {
         $("#haut").fadeOut(2000);
     });
 });
+
+function home_to_chrono() {
+    $("#chrono_section").show("fast");
+    $("#home_to_chrono").hide("fast");
+}
+
+function chrono_to_stakes() {
+    $("#stakes_section").show("fast");
+    $("#chrono_to_stakes").hide("fast");
+}
+
+function stakes_to_answer() {
+    $("#answer_section").show("fast");
+    $("#stakes_to_answer").hide("fast");
+}
+
+function answer_to_map() {
+    $("#map_section").show("fast");
+    $("#answer_to_map").hide("fast");
+}
+
+function home_section() {
+    $("#home_section").show("fast");
+    //$("#answer_to_map").hide("fast");
+}
